92e34192aeb9aa8df610e4dafa7676c2ec9250f4
Rodrigo Thauã <rodrigothaua@gmail.com>
2025-04-14 08:41:58 -0400

    Ajuste em index

--- resources/views/desenvolvimento/index.blade.php
+++ resources/views/desenvolvimento/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Desenvolvimento')
+@section('title', 'Desenvolvimento - SIGECOM')
 
 @section('sidebar')
     @include('layouts.sidebar')
@@ -9,7 +9,7 @@
 @section('content')
     <div class="container-fluid">
         <h3>Carlos Cury Tito
-            <small class="text-muted">Idealizador e Analisa do sistema</small>
+            <small class="text-muted">Idealizador e Analista do sistema</small>
         </h3>
         <h3>Rodrigo Thauã Lima Barroso
             <small class="text-muted">Desenvolvimento e Programação</small>
